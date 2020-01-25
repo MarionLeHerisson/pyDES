77d43438b96ebd0c89befc0beb17aa4a5cc68994
robin <robin.sgeorges@gmail.com>
2020-01-25 02:45:49 +0100

    getValueXY tested and working as in course exemple

--- des.py
+++ des.py
@@ -141,7 +141,32 @@ def get8BlocsOf6Bits(EDKi):
     result[acc] = stringTemp
     return result
 
+#Prend un dict et renvoi un tableau en deux dimensions pour chercher une valeur plus facilement
+def getMatrixFromDictio(dictio):
+    x = 16
+    y = 4
+    matrix = {}
+    for i in range(0, len(dictio)):
+        tempY = 1
+        if(i<16):
+            tempY=1
+        elif(i<32):
+            tempY=2
+        elif(i<48):
+            tempY=3
+        else:
+            tempY=4
+        #print( "x : " + str((i%16)+1) + " y :" +str(tempY))
+        if(matrix.get(i % 16 + 1) == None):
+            matrix[i % 16 + 1] = {}
+        matrix[i % 16 +1][tempY] = dictio[i]
+        #print("val to get : "+ str(dictio[i]))
+        #print(matrix[i % 16 +1][tempY] )
+    return matrix
 
+def getValueXY(dico, x, y):
+    matrix = getMatrixFromDictio(dico)
+    return matrix[x][y]
 
 def ronde(D, keys):
     for i in range(0,16):
@@ -245,5 +270,7 @@ def encode_des():
 #encode_des()
 ## TESTS ##
 #get16KeysFromKey("11000000000111110100100011110010111101001001011010111111")
-lesBlocs = get8BlocsOf6Bits("110001100111111100101010010101111000111101001101")
-print(lesBlocs)
+#lesBlocs = get8BlocsOf6Bits("110001100111111100101010010101111000111101001101")
+#print(lesBlocs)
+
+print(getValueXY(S1, 16, 4))
