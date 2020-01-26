9e25e8134e053c154271482c54f10d16aad25621
robin <robin.sgeorges@gmail.com>
2020-01-26 17:59:12 +0100

    txt2String working

--- ConvAlphaBin.py
+++ ConvAlphaBin.py
@@ -3,80 +3,118 @@
 #####################################################
 
 ALPHABET = ""
-ALPHABET+="ABCDEFGHIJKLMNOPQRSTUVWXYZ"#Les 26 lettres de l'alphabet en majuscule (0-25)
-ALPHABET+="abcdefghijklmnopqrstuvwxyz"#Les 26 lettres de l'alphabet en minuscule (26-51)
-ALPHABET+=" " #caractère 52 : espace
-ALPHABET+="." #caractère 53 : point
-ALPHABET+="," #caractère 54 : virgule
-ALPHABET+="!" #caractère 55 : point d'exclamation
-ALPHABET+="?" #caractère 56 : point d'intérogation
-ALPHABET+="'" #caractère 57 : apostrophe
-ALPHABET+='"' #caractère 58 : guillemet
-ALPHABET+="é" #caractère 59 : e accent aïgu
-ALPHABET+="è" #caractère 60 : e accent grave
-ALPHABET+="à" #caractère 61 : a accent aïgu
-ALPHABET+="-" #caractère 62 : tiret
-ALPHABET+="\n" #caractère 63 : saut de ligne
+ALPHABET += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  # Les 26 lettres de l'alphabet en majuscule (0-25)
+ALPHABET += "abcdefghijklmnopqrstuvwxyz"  # Les 26 lettres de l'alphabet en minuscule (26-51)
+ALPHABET += " "  # caractère 52 : espace
+ALPHABET += "."  # caractère 53 : point
+ALPHABET += ","  # caractère 54 : virgule
+ALPHABET += "!"  # caractère 55 : point d'exclamation
+ALPHABET += "?"  # caractère 56 : point d'intérogation
+ALPHABET += "'"  # caractère 57 : apostrophe
+ALPHABET += '"'  # caractère 58 : guillemet
+ALPHABET += "é"  # caractère 59 : e accent aïgu
+ALPHABET += "è"  # caractère 60 : e accent grave
+ALPHABET += "à"  # caractère 61 : a accent aïgu
+ALPHABET += "-"  # caractère 62 : tiret
+ALPHABET += "\n"  # caractère 63 : saut de ligne
+
+# Dans cet alphabet les caractères vont de 0 à 63.
+# En binaire ça donne un champ de valeur de 0 à 111111(=1+2+4+8+16+32=63)
+# ALPHABETBINAIRE est un tableau avec les nombres en binaire
+ALPHABETBINAIRE = dict()
+for i in range(0, 64):
+    x = bin(i)
+    y = '00000' + x[2:]
+    ALPHABETBINAIRE[i] = ""
+    for k in range(-6, 0, -1):
+        ALPHABETBINAIRE[i] += y[k]
 
 
-#Dans cet alphabet les caractères vont de 0 à 63.
-#En binaire ça donne un champ de valeur de 0 à 111111(=1+2+4+8+16+32=63)
-#ALPHABETBINAIRE est un tableau avec les nombres en binaire
-ALPHABETBINAIRE=dict()
-for i in range(0, 64) :
-	x=bin(i)
-	y='00000'+x[2:]
-	ALPHABETBINAIRE[i]=""
-	for k in range(-6, 0, -1) :
-	    ALPHABETBINAIRE[i]+=y[k]
+def txt2String(s_str):
+    binary = []
+    for s in s_str:
+        if s == ' ':
+            binary.append('00100000')
+        else:
+            binary.append(bin(ord(s)))
+    b_str = ''.join(str(b_str) for b_str in binary)
+    return b_str.replace("b","")
 
-#Renvoie la chaine de caractère txt avec uniquement les caractères de l'alphabet.
-def FiltreTXT(txt) :
-	res=""
-	for c in txt :
-		if(ALPHABET.find(c)!=-1) : res+=c
-		elif(c=='ê' or c=='ë') : res+='e'
-		elif(c=='â') : res+='a'
-		elif(c=='ç') : res+='c'
-		elif(c=='î') : res+='i'
-		elif(c=='Ç') : res+='C'
-		elif(c=='ù' or c=="û") : res+='u'
-		elif(c=='ô') : res+='o'
-		elif(c=='Ô') : res+='O'
-		elif(c=='œ') : res+='oe'
-		elif(c=="À") : res+='A'
-		elif(c=="È" or c=="É") : res+='E'
-	return res
-	
-#Prend en paramètre un texte et renvoie la chaine binaire associée (en suivant le dictionnaire)
-def str_to_bin(txt) :
-	X=""
-	for c in FiltreTXT(txt) : 
-		i=ALPHABET.find(c)
-		if(i!=-1) : X+=ALPHABETBINAIRE[i]
-	return X
 
-#Fait l'inverse de conv_bin : prend une chaine binaire et renvoie les caractères
-def bin_to_str(txt) :
-	n=len(txt)
-	res=""
-	i=0
-	while(i<n or i%6!=0) : 
-		if(i%6==0) : paquet_binaire="" 
-		try : c=txt[i]
-		except : c="0"
-		if(c=="1") : paquet_binaire+="1"
-		else : paquet_binaire+="0"
-		i+=1
-		if(i%6==0) : 
-			for b in ALPHABETBINAIRE : 
-				if(ALPHABETBINAIRE[b]==paquet_binaire) :
-					res+=ALPHABET[b]
-					break
-	
-	return res
+def conv_bin(txt):
+    bin_txt = ""
+    for char in FiltreTXT(txt):
+        bin_idx = ALPHABET.find(char)
+        if bin_idx != -1:
+            bin_txt += ALPHABETBINAIRE[bin_idx]
+    return bin_txt
 
-#Test
+
+# Renvoie la chaine de caractère txt avec uniquement les caractères de l'alphabet.
+def FiltreTXT(txt):
+    res = ""
+    for c in txt:
+        if (ALPHABET.find(c) != -1):
+            res += c
+        elif (c == 'ê' or c == 'ë'):
+            res += 'e'
+        elif (c == 'â'):
+            res += 'a'
+        elif (c == 'ç'):
+            res += 'c'
+        elif (c == 'î'):
+            res += 'i'
+        elif (c == 'Ç'):
+            res += 'C'
+        elif (c == 'ù' or c == "û"):
+            res += 'u'
+        elif (c == 'ô'):
+            res += 'o'
+        elif (c == 'Ô'):
+            res += 'O'
+        elif (c == 'œ'):
+            res += 'oe'
+        elif (c == "À"):
+            res += 'A'
+        elif (c == "È" or c == "É"):
+            res += 'E'
+    return res
+
+
+# Prend en paramètre un texte et renvoie la chaine binaire associée (en suivant le dictionnaire)
+def str_to_bin(txt):
+    X = ""
+    for c in FiltreTXT(txt):
+        i = ALPHABET.find(c)
+        if (i != -1): X += ALPHABETBINAIRE[i]
+    return X
+
+
+# Fait l'inverse de conv_bin : prend une chaine binaire et renvoie les caractères
+def bin_to_str(txt):
+    n = len(txt)
+    res = ""
+    i = 0
+    while (i < n or i % 6 != 0):
+        if (i % 6 == 0): paquet_binaire = ""
+        try:
+            c = txt[i]
+        except:
+            c = "0"
+        if (c == "1"):
+            paquet_binaire += "1"
+        else:
+            paquet_binaire += "0"
+        i += 1
+        if (i % 6 == 0):
+            for b in ALPHABETBINAIRE:
+                if (ALPHABETBINAIRE[b] == paquet_binaire):
+                    res += ALPHABET[b]
+                    break
+
+    return res
+
+# Test
 # txt0 = "Je teste au stérone !? ^_^"
 # txt1 = str_to_bin(txt0)
 # txt2 = bin_to_str(txt1)
